7bdece31edf2a32b576f39e316b423a0bdb30b59
David Eastcroft <davideastcroft95@gmail.com>
2020-11-27 20:59:02 +0000

    Adds likes feature.

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
@@ -27,7 +27,7 @@ module.exports = {
 		}
 	},
 	Mutation: {
-		async createPost(_, { body }, context){
+		async createPost(_, { body }, context) {
 			const user = checkAuth(context);
 
 			console.log(user);
@@ -59,6 +59,23 @@ module.exports = {
 				throw new Error(err);
 			}
 			
+		},
+		async likePost(_, { postId }, context) {
+			const { username } = checkAuth(context);
+
+			const post = await Post.findById(postId);
+			if(post){
+				if(post.likes.find(like => like.username === username)){
+					post.likes = post.likes.filter(like => like.username !== username);
+				} else {
+					post.likes.push({
+						username,
+						createdAt: new Date().toISOString()
+					})
+				}
+				await post.save();
+				return post;
+			} else throw new UserInputError('Post not found.');
 		}
 	}
 };
