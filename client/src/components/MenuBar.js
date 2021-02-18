ad665e6e3e44aafdbcd44960713620b6518f0e44
David Eastcroft <davideastcroft95@gmail.com>
2021-02-18 12:58:33 +0000

    Authentication context implemented, login working, redirect for login/register pages when logged in.

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,20 +1,36 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../context/auth';
+
 function MenuBar() {
-  
+  const { user, logout } = useContext(AuthContext)
+
 	const pathName = window.location.pathname;
 	const path = pathName === '/' ? 'home' : pathName.substr(1);
 
 	const [activeItem, setActiveItem] = useState(path);
 
 	const handleItemClick = (e, { name }) => setActiveItem(name);
-	
-	
 
-  return (
-    <Menu pointing secondary size="massive" color="blue">
+	const menuBar = user ? (
+		<Menu pointing secondary size="massive" color="blue">
+      <Menu.Item
+        name={ user.username }
+        active
+				as={Link}
+				to="/"
+      />
+      <Menu.Menu position='right'>
+				<Menu.Item
+          name='logout'
+					onClick={logout}
+        />
+      </Menu.Menu>
+    </Menu>
+	) : (
+		<Menu pointing secondary size="massive" color="blue">
       <Menu.Item
         name='home'
         active={activeItem === 'home'}
@@ -39,7 +55,9 @@ function MenuBar() {
         />
       </Menu.Menu>
     </Menu>
-  )
+	)
+
+  return menuBar
 }
 
 
